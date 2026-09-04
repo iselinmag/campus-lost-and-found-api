a94837345812a29ffb66b996f46c472126fa1434
iselinmag <iselinsophie99@outlook.com>
2026-09-04 14:19:20 +0200

    Add basic lost items API routes

--- src/server.js
+++ src/server.js
@@ -2,10 +2,43 @@ const fastify = require('fastify')({
   logger: true
 });
 
-fastify.get('/', async (request, reply) => {
+const items = [
+  {
+    id: 1,
+    name: 'Black Backpack',
+    description: 'Found near the library',
+    date: '2026-09-01'
+  },
+  {
+    id: 2,
+    name: 'AirPods',
+    description: 'Found in classroom B204',
+    date: '2026-09-02'
+  }
+];
+
+fastify.get('/', async () => {
   return {
     message: 'Campus Lost-and-Found API is running'
   };
+});
+
+fastify.get('/items', async () => {
+  return items;
+});
+
+fastify.get('/items/:id', async (request, reply) => {
+  const id = Number(request.params.id);
+
+  const item = items.find(item => item.id === id);
+
+  if (!item) {
+    return reply.code(404).send({
+      error: 'Item not found'
+    });
+  }
+
+  return item;
 });
 
 const start = async () => {
